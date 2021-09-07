c34bf3fc71ee127886065bf98ae202ad1c4c4eef
Evening <johnchangqi@hotmail.com>
2021-09-08 02:10:30 +0800

    Update missing standards onto Player2D

--- Assets/Player2D.cs
+++ Assets/Player2D.cs
@@ -4,54 +4,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player2D : MonoBehaviour
+namespace DIPProject
 {
-    public PhotonView photonView;
-    public Text playerNameText;
-    public GameObject playerCamera;
-
-
-    public float speed = 5;
-
-    private Rigidbody2D rb;
-    private Vector2 moveVelocity;
-
-
-    private void Awake()
+    /// <summary>
+    /// This controls the player in 2D environments, specifically in the Landing.
+    /// </summary>
+    public class Player2D : MonoBehaviour
     {
-        PhotonNetwork.NickName = PhotonNetwork.NickName == "" ? "Unknown Name" : PhotonNetwork.NickName;
 
-        playerCamera.SetActive(photonView.IsMine);
-        if (photonView.IsMine)
+		#region Variables
+
+		public PhotonView photonView;
+        public Text playerNameText;
+
+        /// <summary>
+        /// Not too sure if this is needed? The 2D scene is not yet multiplayer?
+        /// I'll leave this here just in case we want players too see each other in lobby?
+        /// </summary>
+        public GameObject playerCamera;
+
+        [Tooltip("Controls how fast the player is moving in 2D.")]
+        [SerializeField]
+        public float speed = 5;
+
+        private Rigidbody2D rb;
+        private Vector2 moveVelocity;
+
+		#endregion
+
+		#region MonoBehavior Callbacks
+		private void Awake()
         {
-            playerNameText.text = PhotonNetwork.NickName;
+            PhotonNetwork.NickName = PhotonNetwork.NickName == "" ? "Unknown Name" : PhotonNetwork.NickName;
+
+            playerCamera.SetActive(photonView.IsMine);
+            if (photonView.IsMine)
+            {
+                playerNameText.text = PhotonNetwork.NickName;
+            }
+            else
+            {
+                playerNameText.text = photonView.Owner.NickName == "" ? photonView.Owner.NickName : "Unexpected Missing Name";
+            }
+
         }
-        else
+
+        // Start is called before the first frame update
+        void Start()
         {
-            playerNameText.text = photonView.Owner.NickName == "" ? photonView.Owner.NickName : "Unexpected Missing Name";
+            rb = GetComponent<Rigidbody2D>();
         }
 
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (photonView.IsMine)
+        // Update is called once per frame
+        void Update()
         {
-            Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-            moveVelocity = moveInput * speed;
+            if (photonView.IsMine)
+            {
+                Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+                moveVelocity = moveInput * speed;
+            }
         }
-    }
 
-    private void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
-    }
+        private void FixedUpdate()
+        {
+            rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        }
 
+		#endregion
+
+	}
 }
