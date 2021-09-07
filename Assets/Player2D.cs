75ef20633536e42b688223af38285327815850f8
Evening <johnchangqi@hotmail.com>
2021-09-08 01:44:48 +0800

    Drop old scripts

--- Assets/Player2D.cs
+++ Assets/Player2D.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+public class Player2D : MonoBehaviour
 {
     public PhotonView photonView;
     public Text playerNameText;
