ca3a1a641082b669f57c8f1e24886e73d8f0e9a4
Evening <johnchangqi@hotmail.com>
2021-09-12 21:09:06 +0800

    Update Chat manager to call coroutine popup

--- Assets/PhotonChatManager.cs
+++ Assets/PhotonChatManager.cs
@@ -3,131 +3,155 @@ using Photon.Chat;
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PhotonChatManager : MonoBehaviour, IChatClientListener
-{
-    ChatClient chatClient;
-	[Tooltip("The InputField for sending a message")]
-	[SerializeField] InputField chatInput;
-	[Tooltip("The TextWindow")]
-	[SerializeField] Text chatWindow;
-	[SerializeField] Button chatBtn;
-
-	#region IChatClientListener Callbacks
-
-	public void DebugReturn(DebugLevel level, string message) {	}
-
-	public void OnChatStateChange(ChatState state)	{ }
-
-	public void OnConnected()
+namespace DIPProject {
+	public class PhotonChatManager : MonoBehaviour, IChatClientListener
 	{
-		chatClient.Subscribe(new string[] { PhotonNetwork.CurrentRoom.Name });
-		ToggleChat(true);
-	}
+		ChatClient chatClient;
+		[Tooltip("The InputField for sending a message")]
+		[SerializeField] InputField chatInput;
+		[Tooltip("The TextWindow")]
+		[SerializeField] Text chatWindow;
+		[SerializeField] Button chatBtn;
 
-	public void OnDisconnected()
-	{
-		chatClient.Unsubscribe(new string[] { PhotonNetwork.CurrentRoom.Name });
-		ToggleChat(false);
-	}
+		#region IChatClientListener Callbacks
 
-	public void OnGetMessages(string channelName, string[] senders, object[] messages)
-	{
-		if (channelName == PhotonNetwork.CurrentRoom.Name) AppendChat(senders, messages);
-	}
+		public void DebugReturn(DebugLevel level, string message) { }
 
-	public void OnPrivateMessage(string sender, object message, string channelName)
-	{
-		//throw new System.NotImplementedException();
-	}
+		public void OnChatStateChange(ChatState state) { }
 
-	public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
-	{
-		//throw new System.NotImplementedException();
-	}
-
-	public void OnSubscribed(string[] channels, bool[] results)
-	{
-		MsgRoom(PhotonNetwork.NickName + " has connected to the channel!");
-	}
-
-	public void OnUnsubscribed(string[] channels)
-	{
-		MsgRoom(PhotonNetwork.NickName + " has disconnected from the channel!");
-	}
-
-	public void OnUserSubscribed(string channel, string user)
-	{
-		Debug.Log(user + " has subscribed to " + channel);
-	}
-
-	public void OnUserUnsubscribed(string channel, string user)
-	{
-		Debug.Log(user + " has unsubscribed from " + channel);
-	}
-
-	#endregion
-
-	#region MonoBehavior Callbacks
-
-	// Start is called before the first frame update
-	void Start()
-    {
-		ToggleChat(false);
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-		if (PhotonNetwork.InRoom)
+		public void OnConnected()
 		{
-			if (chatClient == null) Connect();
-			chatClient.Service();
+			chatClient.Subscribe(new string[] { PhotonNetwork.CurrentRoom.Name });
+			ToggleChat(true);
 		}
-    }
 
-	#endregion
-
-	#region Public Methods
-	public void Connect()
-	{
-		Debug.Log("Photon Chat attempting to Connect");
-		chatClient = new ChatClient(this);
-		chatClient.Connect(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat, PhotonNetwork.AppVersion, new AuthenticationValues(PhotonNetwork.NickName));
-	}
-
-	void MsgRoom(string msg)
-	{
-		bool result = chatClient.PublishMessage(PhotonNetwork.CurrentRoom.Name, msg);
-
-		Debug.Log("Attempting to send message " + msg + " to " + PhotonNetwork.CurrentRoom.Name);
-		if (!result) Debug.Log("Failed to send message");
-	}
-
-	public void SendChatMsg()
-	{
-		MsgRoom(chatInput.text);
-		chatInput.text = "";
-	}
-
-	void ToggleChat(bool status)
-	{
-		chatBtn.enabled = status;
-		chatInput.enabled = status;
-		chatWindow.enabled = status;
-	}
-
-	public void AppendChat(string[] senders, object[] messages)
-	{
-		for (int i = 0; i < senders.Length; i++)
+		public void OnDisconnected()
 		{
-			chatWindow.text += "\n" + senders[i] + ": " + messages[i];
+			chatClient.Unsubscribe(new string[] { PhotonNetwork.CurrentRoom.Name });
+			ToggleChat(false);
 		}
+
+		public void OnGetMessages(string channelName, string[] senders, object[] messages)
+		{
+			if (channelName == PhotonNetwork.CurrentRoom.Name) {
+				AppendChat(senders, messages);
+				CreateChatPopUp(senders, messages);
+			}
+		}
+
+		public void OnPrivateMessage(string sender, object message, string channelName)
+		{
+			//throw new System.NotImplementedException();
+		}
+
+		public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
+		{
+			//throw new System.NotImplementedException();
+		}
+
+		public void OnSubscribed(string[] channels, bool[] results)
+		{
+			MsgRoom(PhotonNetwork.NickName + " has connected to the channel!");
+		}
+
+		public void OnUnsubscribed(string[] channels)
+		{
+			MsgRoom(PhotonNetwork.NickName + " has disconnected from the channel!");
+		}
+
+		public void OnUserSubscribed(string channel, string user)
+		{
+			Debug.Log(user + " has subscribed to " + channel);
+		}
+
+		public void OnUserUnsubscribed(string channel, string user)
+		{
+			Debug.Log(user + " has unsubscribed from " + channel);
+		}
+
+		#endregion
+
+		#region MonoBehavior Callbacks
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			ToggleChat(false);
+		}
+
+
+		// Update is called once per frame
+		void Update()
+		{
+			if (PhotonNetwork.InRoom)
+			{
+				if (chatClient == null) Connect();
+
+				if (Input.GetKeyDown(KeyCode.Return)) SendChatMsg();
+				chatClient.Service();
+			}
+
+		}
+
+
+		#endregion
+
+		#region Public Methods
+		public void Connect()
+		{
+			Debug.Log("Photon Chat attempting to Connect");
+			chatClient = new ChatClient(this);
+			chatClient.Connect(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat, PhotonNetwork.AppVersion, new AuthenticationValues(PhotonNetwork.NickName));
+		}
+
+		void MsgRoom(string msg)
+		{
+			bool result = chatClient.PublishMessage(PhotonNetwork.CurrentRoom.Name, msg);
+
+			Debug.Log("Attempting to send message " + msg + " to " + PhotonNetwork.CurrentRoom.Name);
+			if (!result) Debug.Log("Failed to send message");
+		}
+
+		public void SendChatMsg()
+		{
+			if (chatInput.text.Trim() == "") return;
+			MsgRoom(chatInput.text);
+			chatInput.text = "";
+		}
+
+		void ToggleChat(bool status)
+		{
+			chatBtn.enabled = status;
+			chatInput.enabled = status;
+			chatWindow.enabled = status;
+		}
+
+		public void AppendChat(string[] senders, object[] messages)
+		{
+			for (int i = 0; i < senders.Length; i++)
+			{
+				chatWindow.text += "\n" + senders[i] + ": " + messages[i];
+			}
+		}
+
+
+		public void CreateChatPopUp(string[] senders, object[] messages)
+		{
+			GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+			foreach (GameObject player in players)
+			{
+				for (int i = 0; i < senders.Length; i ++)
+				{
+					if (player.GetPhotonView().Owner.NickName == senders[i])
+						StartCoroutine(player.GetComponent<Player2D>().ChatPopup(messages[i]));
+				}
+			}
+		}
+
+		#endregion
 	}
-
-
-	#endregion
 }
