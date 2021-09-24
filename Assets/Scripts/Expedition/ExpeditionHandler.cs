c2464cca1e0bd775661d569f814dae8f3d3e15cd
Evening <johnchangqi@hotmail.com>
2021-09-24 22:37:09 +0800

    Rename Expedition Handlers

--- Assets/Scripts/Expedition/ExpeditionHandler.cs
+++ Assets/Scripts/Expedition/ExpeditionHandler.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 namespace DIPProject
 {
-    public class ExpeditionManager : MonoBehaviourPunCallbacks
+    public class ExpeditionHandler : MonoBehaviourPunCallbacks
     {
 		#region Variables
 		[SerializeField]
