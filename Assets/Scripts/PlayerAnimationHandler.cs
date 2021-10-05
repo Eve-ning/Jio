7d7faca0046becdb9b8e77a43295733d7b9a95bf
Evening <johnchangqi@hotmail.com>
2021-10-05 17:40:07 +0800

    Fix issue with RPC being called every frame

--- Assets/Scripts/PlayerAnimationHandler.cs
+++ Assets/Scripts/PlayerAnimationHandler.cs
@@ -15,6 +15,10 @@ namespace DIPProject
         private static readonly int Stop = Animator.StringToHash("Stop");
         private static readonly int Active = Animator.StringToHash("Active");
 
+        // This is to keep track of the previous movement state so that the calls
+        // are not redundant.
+        private static Movement currentMovement = Movement.Stop;
+        
         [SerializeField] private Animator animator;
         [SerializeField] private PhotonView photonView;
 
@@ -30,22 +34,26 @@ namespace DIPProject
         #endregion
         #region MonoBehavior Callbacks
 
-        // Update is called once per frame
         private void Start()
         {
             photonView = PhotonView.Get(this);
         }
 
+        // Update is called once per frame
         private void Update()
         {
             // On execute the input animation handler if the photon view is ours
             if (photonView.IsMine) InputToTrigger();
         }
 
+        /// <summary>
+        /// Gets the local input and calls the appropriate animations
+        /// Will avoid calling RPC if the client is not in a room.
+        /// </summary>
         private void InputToTrigger()
         {
             // We will get the player's input and trigger the corresponding animation
-            if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0) 
+            if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
                 AnimationTrigger(Movement.Stop);
             else
             {
@@ -59,8 +67,23 @@ namespace DIPProject
         #endregion
         #region Animation Triggers
 
+        /// <summary>
+        /// Triggers the animation based on the current movement
+        /// Will avoid redundant movement triggers if possible.
+        /// </summary>
+        /// <param name="movement">The current movement</param>
         private void AnimationTrigger(Movement movement)
         {
+            // If the current movement is the same as before, don't do anything more
+            // Else, we update our current movement,
+            // then we call the RPC and local animation triggers!
+            //
+            // We do this because Update will repeatedly call redundant RPCs, causing
+            // many glitches.
+            if (currentMovement == movement) return;
+            
+            currentMovement = movement;
+
             // In addition to setting the animator triggers
             // We also send the RPC if we are in a room
             // Note that we shouldn't send RPCs in the lobby / camp
@@ -104,6 +127,14 @@ namespace DIPProject
         
         #region RPC Callbacks
 
+        /// <summary>
+        /// The receiver of the RPC.
+        /// This will only be called by the non-callee as RPC is set to Others.
+        /// We will thus then loop through all the clients and find the
+        /// photonView owner of the local instance and trigger their animation.
+        /// </summary>
+        /// <param name="movementId"></param>
+        /// <param name="info"></param>
         [PunRPC]
         [UsedImplicitly]
         void RPCTrigger(int movementId, PhotonMessageInfo info)
